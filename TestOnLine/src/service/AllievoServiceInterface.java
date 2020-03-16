3b03749f60eb491897fa5a0404bf3620a28c5235
Angelo <Angelo@Gnagno.station>
2020-03-16 12:05:01 +0100

    modifica allievo service interface

--- TestOnLine/src/service/AllievoServiceInterface.java
+++ TestOnLine/src/service/AllievoServiceInterface.java
@@ -1,5 +1,16 @@
 package service;
 
+import entity.Allievo;
+
 public interface AllievoServiceInterface {
-//
+	
+	public void registraAllievo(Allievo a) throws Exception;
+	public Allievo leggiDatiAllievo(String username) throws Exception;
+	public void cancellaAllievo(String username) throws Exception;
+	public void modificaPasswordAllievo(Allievo a, String newPass) throws Exception;
+	public void modificaNomeAllievo(Allievo a, String newNome) throws Exception;
+	public void modificaCognomeAllievo(Allievo a, String newCognome) throws Exception;
+	public void modificaEmailAllievo(Allievo a, String newEmail) throws Exception;
+	
+
 }
--- TestOnLine/src/service/QuizServiceInterface.java
+++ TestOnLine/src/service/QuizServiceInterface.java
@@ -1,5 +1,16 @@
 package service;
 
+import java.util.List;
+
+import entity.Allievo;
+
+
 public interface QuizServiceInterface {
-//
+	
+	public void registraUtente(Allievo a) throws Exception;
+	public Allievo leggiDatiUtente(String username) throws Exception;
+	public void cancellaUtente(String username) throws Exception;
+	public void modificaPasswordUtente(String username, String newPass) throws Exception;
+	
+
 }
