206671b9778dc6d7a01c99c1e56e652830b0afdc
Angelo <Angelo@Gnagno.station>
2020-03-16 12:16:46 +0100

    quiz service interface

--- TestOnLine/src/service/QuizServiceInterface.java
+++ TestOnLine/src/service/QuizServiceInterface.java
@@ -3,14 +3,17 @@ package service;
 import java.util.List;
 
 import entity.Allievo;
+import entity.Quiz;
 
 
 public interface QuizServiceInterface {
 	
-	public void registraUtente(Allievo a) throws Exception;
-	public Allievo leggiDatiUtente(String username) throws Exception;
-	public void cancellaUtente(String username) throws Exception;
-	public void modificaPasswordUtente(String username, String newPass) throws Exception;
-	
+	public void registraQuiz(Quiz q);
+	public List<Quiz> selectAll();
+	public List<Quiz> selectQuizAllievo(Allievo a);
+	public Quiz select(int id);
+	public List<Quiz> selectQuizArgomento(String argomento);
+	public void update(Quiz q);
+	public void delete(Quiz q);
 
 }
