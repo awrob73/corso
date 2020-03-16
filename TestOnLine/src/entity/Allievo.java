c502f5920d1537f845ce11a59c5993d915b80f6c
Andrea <Andrea@DESKTOP-E7R9V49.lan>
2020-03-16 11:46:38 +0100

    modifica Allievo

--- TestOnLine/src/entity/Allievo.java
+++ TestOnLine/src/entity/Allievo.java
@@ -3,4 +3,50 @@ package entity;
 public class Allievo {
 
 	private String username;
+	
+	private String nome;
+	
+	private String cognome;
+	
+	private String email;
+	
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "Allievo [username=" + username + ", nome=" + nome + ", cognome=" + cognome + ", email=" + email
+				+ ", password=" + password + "]";
+	}
+	
 }
