ebd2788fc432e26f485160f44f0a547709fe1da9
Alexander <Alexander@DESKTOP-AIO19GE.station>
2020-03-17 10:10:58 +0100

    Creazione modficaAllievoServlet Creazione AllievoSeriviceImpl--da riempire

---
+++ TestOnLine/src/service/AllievoServiceImpl.java
@@ -0,0 +1,55 @@
+package service;
+
+import entity.Allievo;
+
+public class AllievoServiceImpl implements AllievoServiceInterface {
+
+	@Override
+	public void registraAllievo(Allievo a) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Allievo leggiDatiAllievo(String username) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cancellaAllievo(String username) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modificaPassword(Allievo a, String newPass) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modificaNome(Allievo a, String newNome) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modificaCognome(Allievo a, String newCognome) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modificaEmail(Allievo a, String newEmail) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modificaStato(Allievo a, String newStato) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
