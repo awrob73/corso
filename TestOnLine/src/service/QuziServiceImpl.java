d0904ecb3ad0c1e7d81cfe4980887197d50bfdaf
Alexander <Alexander@DESKTOP-AIO19GE.station>
2020-03-17 10:19:09 +0100

    Creazione classi ServiceImpl di Quiz e Report (daRiempire)

---
+++ TestOnLine/src/service/QuziServiceImpl.java
@@ -0,0 +1,74 @@
+package service;
+
+import java.util.List;
+
+import entity.Allievo;
+import entity.Argomento;
+import entity.Domanda;
+import entity.Quiz;
+import entity.Risposta;
+
+public class QuziServiceImpl implements QuizServiceInterface {
+
+	@Override
+	public void registraQuiz(Quiz q) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Quiz> selectAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Quiz> selectQuizAllievo(Allievo a) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Quiz selectQuiz(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Argomento> selectAllArgomento() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Quiz> selectQuizArgomento(String argomento) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateQuiz(Quiz q) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteQuiz(Quiz q) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Domanda> selectAllByQuiz(Quiz q) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Risposta> selectAllByDomanda(Domanda d) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
---
+++ TestOnLine/src/service/ReportServiceImpl.java
@@ -0,0 +1,34 @@
+package service;
+
+import java.util.List;
+
+import entity.Allievo;
+import entity.Report;
+
+public class ReportServiceImpl implements ReportServiceInterface {
+
+	@Override
+	public void createReport(Report r) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Report> readReport(Allievo a) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Report> readAllReport() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Report readReport(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
