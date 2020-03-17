320ccee066301c697fe48b9e27acce0f0dabc14f
Andrea <Andrea@DESKTOP-E7R9V49.lan>
2020-03-17 09:28:11 +0100

    modifiche ServiceInterface

--- TestOnLine/src/service/QuizServiceInterface.java
+++ TestOnLine/src/service/QuizServiceInterface.java
@@ -3,7 +3,10 @@ package service;
 import java.util.List;
 
 import entity.Allievo;
+import entity.Argomento;
+import entity.Domanda;
 import entity.Quiz;
+import entity.Risposta;
 
 
 public interface QuizServiceInterface {
@@ -16,10 +19,16 @@ public interface QuizServiceInterface {
 	
 	public Quiz selectQuiz(int id) throws Exception;
 	
+	public List<Argomento> selectAllArgomento() throws Exception;
+	
 	public List<Quiz> selectQuizArgomento(String argomento) throws Exception;
 	
 	public void updateQuiz(Quiz q) throws Exception;
 	
 	public void deleteQuiz(Quiz q) throws Exception;
+	
+	public List<Domanda> selectAllByQuiz(Quiz q) throws Exception;
+	
+	public List<Risposta> selectAllByDomanda(Domanda d) throws Exception;
 
 }
--- TestOnLine/src/service/ReportServiceInterface.java
+++ TestOnLine/src/service/ReportServiceInterface.java
@@ -2,13 +2,14 @@ package service;
 
 import java.util.List;
 
+import entity.Allievo;
 import entity.Report;
 
 public interface ReportServiceInterface {
 
 	public void createReport(Report r) throws Exception;
 	
-	public List<Report> readReport(String username) throws Exception;
+	public List<Report> readReport(Allievo a) throws Exception;
 	
 	public List<Report> readAllReport() throws Exception;
 	
