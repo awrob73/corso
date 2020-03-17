522e43c9f09ff80baf818c2ac853f71956d94aeb
Hp <pigna.emanuele@gmail.com>
2020-03-17 15:25:58 +0100

    Aggiunta loginservlet

--- TestOnLine/src/servlet/LoginServlet.java
+++ TestOnLine/src/servlet/LoginServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import entity.Allievo;
 import service.AllievoServiceImpl;
 
-
+//modifiche
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
