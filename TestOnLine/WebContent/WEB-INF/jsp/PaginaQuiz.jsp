dd9f87b2d0021b85d1c6f058187a62fa6c08c94e
Angelo <Angelo@Gnagno.station>
2020-03-18 12:09:59 +0100

    getquizservice e paginaquizservlet

---
+++ TestOnLine/WebContent/WEB-INF/jsp/PaginaQuiz.jsp
@@ -0,0 +1,37 @@
+<%@page import="entity.Allievo"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<%Allievo a = (Allievo)request.getAttribute("allievo"); %>
+ 
+PAGINA QUIZ
+
+<p>
+ 
+Benvenuto <%= a.getNome() %> <%= a.getCognome() %> <br>
+
+<form action="getArgomenti">
+<input type="hidden" name="username" value=<%=a.getUsername()%>>
+<input type="submit" value='Cerca'>
+</form>
+<p>
+<form action="getQuizId">
+<input type="text" name = "Id"> id quiz <br>
+<input type="hidden" name="username" value=<%=a.getUsername()%>>
+<input type="submit" value="Visualizza quiz">
+</form>
+<p>
+<form action="getQuizUtente">
+<input type='hidden' name='username' value=<%=a.getUsername()%>>
+<input type='submit' value='Quiz svolti'>
+</form>
+
+</body>
+</html>
---
+++ TestOnLine/src/servlet/getQuizServlet.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entity.Allievo;
+
+import service.AllievoServiceImpl;
+
+
+/**
+ * Servlet implementation class getQuizServlet
+ */
+@WebServlet("/getQuiz")
+public class getQuizServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String username = request.getParameter("username");
+
+			
+			AllievoServiceImpl asi = new AllievoServiceImpl();
+			Allievo a = asi.leggiDatiAllievo(username);
+
+			
+
+			request.setAttribute("allievo", a);
+
+			getServletContext().getRequestDispatcher("/WEB-INF/jsp/PaginaQuiz.jsp").forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			getServletContext().getRequestDispatcher("/WEB-INF/jsp/errore.jsp").forward(request, response);
+
+		}
+	}
+
+}
