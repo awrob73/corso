6996f383ee66155d826be68dd3b2d1d46b445af5
Alexander <Alexander@DESKTOP-AIO19GE.station>
2020-03-23 17:27:21 +0100

    reportService

--- TestOnLine/src/service/ReportServiceImpl.java
+++ TestOnLine/src/service/ReportServiceImpl.java
@@ -1,34 +1,69 @@
 package service;
 
+import java.sql.Connection;
 import java.util.List;
 
+import dao.DataSource;
+import dao.ReportDAOImpl;
+import dao.RispostaDAOImpl;
 import entity.Allievo;
+import entity.AllievoQuiz;
 import entity.Report;
 
 public class ReportServiceImpl implements ReportServiceInterface {
+	
+	private static ReportServiceImpl instance;
+	private ReportDAOImpl dao;
+	private DataSource dataSource;
+
+	private ReportServiceImpl() throws Exception {
+		this.dataSource = DataSource.getInstance();
+		this.dao = ReportDAOImpl.getInstance();
+	
+	}
+
+	public static ReportServiceImpl getInstance() throws Exception {
+		if (instance == null)
+			instance = new ReportServiceImpl();
+		return instance;
+	}
 
 	@Override
 	public void createReport(Report r) throws Exception {
-		// TODO Auto-generated method stub
-
+		Connection conn = dataSource.getConnection();
+		dao.insert(conn, r);
+		conn.commit();
 	}
 
 	@Override
 	public List<Report> readReport(Allievo a) throws Exception {
-		// TODO Auto-generated method stub
+		Connection conn = dataSource.getConnection();
+		conn.commit();
 		return null;
 	}
 
 	@Override
 	public List<Report> readAllReport() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = dataSource.getConnection();
+		List<Report> lista = dao.selectAll(conn);
+		conn.commit();
+		return lista;
 	}
 
 	@Override
 	public Report readReport(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = dataSource.getConnection();
+		Report r = dao.select(conn, id);
+		conn.commit();
+		return r;
+	}
+
+	@Override
+	public List<Report> readReport(AllievoQuiz aq) throws Exception {
+		Connection conn = dataSource.getConnection();
+		List<Report> lista = dao.selectAll(conn, aq);
+		conn.commit();
+		return lista;
 	}
 
 }
--- TestOnLine/src/service/ReportServiceInterface.java
+++ TestOnLine/src/service/ReportServiceInterface.java
@@ -3,6 +3,7 @@ package service;
 import java.util.List;
 
 import entity.Allievo;
+import entity.AllievoQuiz;
 import entity.Report;
 
 public interface ReportServiceInterface {
@@ -10,6 +11,8 @@ public interface ReportServiceInterface {
 	public void createReport(Report r) throws Exception;
 	
 	public List<Report> readReport(Allievo a) throws Exception;
+	
+	public List<Report> readReport(AllievoQuiz aq) throws Exception;
 	
 	public List<Report> readAllReport() throws Exception;
 	
